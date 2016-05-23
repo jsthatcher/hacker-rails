d0a47cae30f0f80e9d76203eaf41372a19e20b8a
Denny Jovic & Jack Thatcher <pair+dennyjovic.jthatcher96@devbootcamp.com>
2016-05-23 14:14:17 -0500

    got posts and comments working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  get '/' => 'posts#index'
+
+  get 'comment/index'
+
+  get 'post/index'
+
+
+  resources :posts do
+    resources :comments
+  end
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  User.create(
+    full_name: Faker::Superhero.name,
+    email: Faker::Internet.free_email,
+    hashed_password: 'password'
+    )
+end
+
+
+20.times do
+  Post.create(
+    title: Faker::Hacker.noun,
+    content: Faker::Hacker.say_something_smart,
+    user_id: rand(1..6)
+    )
+end
+
+100.times do
+  Comment.create(
+    description: Faker::Hacker.say_something_smart,
+    user_id: rand(1..6),
+    post_id: rand(1..20)
+    )
+end
